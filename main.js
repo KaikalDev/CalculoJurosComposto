4a6ce5055367a3795b8e2e9cc78cf92baebf519e
Kaique <kaiquesimoes58@gmail.com>
2025-10-29 19:27:27 -0300

    criação da função de submit/calculo dos juros

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
   const form = document.getElementById('calcForm');
   const tabela = document.querySelector('#tabelaResultados tbody');
   const limpar = document.getElementById('limpar');
+
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const capital = parseFloat(document.getElementById('capital').value);
+    const taxa = parseFloat(document.getElementById('taxa').value) / 100;
+    const tempo = parseInt(document.getElementById('tempo').value);
+
+    tabela.innerHTML = "";
+
+    let montante = capital;
+    let jurosTotal = 0;
+
+    for (let mes = 1; mes <= tempo; mes++) {
+      const jurosMes = montante * taxa;
+      montante += jurosMes;
+      jurosTotal = montante - capital;
+    }
+  });
